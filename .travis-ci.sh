bbc12b95d44468ce322dcd35de7ba4c2a9130def
Akinori ABE <abe@kb.ecei.tohoku.ac.jp>
2015-08-14 22:22:39 +0900

    Add files for Travis CI

---
+++ .travis-ci.sh
@@ -0,0 +1,30 @@
+# Dependencies
+APT_DEPENDS="opam g++"
+OPAM_DEPENDS="ocamlfind ppx_deriving js_of_ocaml"
+
+# Install OPAM and $APT_DEPENDS
+echo "yes" | sudo add-apt-repository ppa:avsm/ocaml42+opam12
+sudo apt-get update -qq
+sudo apt-get install -qq ${APT_DEPENDS}
+
+# Install OCaml
+export OPAMYES=1
+export OPAMVERBOSE=1
+opam init
+opam switch $OCAML_VERSION
+eval `opam config env`
+
+# Show OCaml and OPAM versions
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+
+# Install $OPAM_DEPENDS
+opam install ${OPAM_DEPENDS}
+
+# Test
+./configure $CONFIG --enable-tests
+make
+make test
